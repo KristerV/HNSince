cecc5e8587e6175b9c583354a930cc04c33d565a
Krister Viirsaar <git@krister.ee>
2020-12-08 18:51:33 +0100

    GitHub CI/CD

--- config/prod.exs
+++ config/prod.exs
@@ -10,11 +10,10 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :hnsince, HNSinceWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "hnsince.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
-# Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :debug
 
 config :hnsince, HNSince.Scheduler,
   schedule: {:extended, "*/30 * * * * *"},
--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,6 +4,13 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
 database_url =
   System.get_env("DATABASE_URL") ||
     raise """
@@ -16,26 +23,11 @@ config :hnsince, HNSince.Repo,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
-
 config :hnsince, HNSinceWeb.Endpoint,
   http: [
-    port: String.to_integer(System.get_env("PORT") || "4000"),
+    port: String.to_integer(System.get_env("PORT") || "4001"),
     transport_options: [socket_opts: [:inet6]]
   ],
   secret_key_base: secret_key_base
 
-# ## Using releases (Elixir v1.9+)
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start each relevant endpoint:
-#
-#     config :hnsince, HNSinceWeb.Endpoint, server: true
-#
-# Then you can assemble a release by calling `mix release`.
-# See `mix help release` for more information.
+config :hnsince, HNSinceWeb.Endpoint, server: true
